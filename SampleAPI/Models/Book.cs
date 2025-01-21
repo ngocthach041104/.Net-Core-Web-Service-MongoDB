a9b42711a89e4e4d4c60572b7d032b6051250bd0
ngocthach041104 <thoitranngocthach30@gmail.com>
2025-01-21 22:57:43 +0700

    Add project files.

---
+++ SampleAPI/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using SampleAPI.Models;
+namespace SampleAPI.Models
+{
+    public class Book
+    {
+        [BsonId] //primary key
+        [BsonRepresentation(BsonType.ObjectId)] //passing paran as string 
+        public string? Id { get; set; }
+        [BsonElement("Name")]
+        public string BookName { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public string Author { get; set; }
+    }
+}
